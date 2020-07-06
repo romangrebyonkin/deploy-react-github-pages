2ee8be6805b3b585aa93671463fba11bb20644cd
Roman Grebyonkin <romargbshka@gmail.com>
2020-07-06 19:11:26 +0300

    Updates

---
+++ precache-manifest.2e9aecd1659a84fffc911293c6b0c950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd520b9ec7de6b682a6ee478b4a49b9",
+    "url": "/deploy-react-github-pages/index.html"
+  },
+  {
+    "revision": "f0c9a8df44af627d6a30",
+    "url": "/deploy-react-github-pages/static/css/main.dbc1f3bb.chunk.css"
+  },
+  {
+    "revision": "0454f58317c93bdbbfc9",
+    "url": "/deploy-react-github-pages/static/js/2.41448ad2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deploy-react-github-pages/static/js/2.41448ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c9a8df44af627d6a30",
+    "url": "/deploy-react-github-pages/static/js/main.f3804042.chunk.js"
+  },
+  {
+    "revision": "44c767af76790966bd0b",
+    "url": "/deploy-react-github-pages/static/js/runtime-main.ae7282c2.js"
+  }
+]);
